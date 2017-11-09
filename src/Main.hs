e283aa0b67feed1d8656adf301f79c32806fd59e
Christian Blades <christian.blades+github@gmail.com>
2017-11-08 23:11:40 -0500

    does the thing
    * Data.Text seems like more work than its worth

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 import System.Environment
+import Data.List (nub, sortBy)
+import Data.Function
+import Data.Text.Lazy as T
+import Data.Text.Lazy.IO as TIO
+import Data.Char (isLetter)
 
 main :: IO ()
 main = do
   [s] <- getArgs
-  f <- readFile s
-  mapM_ putStrLn $ words f
+  f <- TIO.readFile s
+  let wList = wordList f
+  mapM_ (Prelude.putStrLn . showWordCount) $ Prelude.take 5 $ sortWordCount $ wordCount $ wordList f
 
+wordList :: Text -> [Text]
+wordList f = Prelude.map (T.filter isLetter) $ T.words $ toLower f
+
+wordCount :: [Text] -> [(Text, Int)]
+wordCount xs = Prelude.zip xs $ Prelude.map (\x -> Prelude.length (Prelude.filter (== x) xs)) $ nub xs
+
+showWordCount :: (Text, Int) -> String
+showWordCount (word, count) = unpack word ++ ": " ++ show count
+
+sortWordCount = sortBy (flip compare `on` snd)
